028de0b6c02a763ebd9a8ab69a3c1c261db9ab76
whrod <whrodtube@gmail.com>
2023-01-18 22:22:28 +0900

    Feat : 투두 벌금자 기능 수정 14:00 이전 이후 결과값 구분

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,15 @@ const server = new Server();
 server.usePlugin(LoggerPlugin);
 // server.usePlugin(KakaoLinkPlugin, config);
 
+/* TODO:
+- 알림 기능
+- 중복 코드 간소화 (클래스 활용)
+- cmd enum화
+- 카카오링크
+- 주석삭제
+*/
+
+//pingTest
 server.on('message', async (msg) => {
   if (!msg.content.startsWith(prefix)) return;
 
@@ -17,37 +26,88 @@ server.on('message', async (msg) => {
   const cmd = args.shift()?.slice(prefix.length);
 
   if (cmd === 'ping') {
-    /*
-      pingTest
-    */
     const timestamp = Date.now();
+
     await msg.reply('Pong!');
     msg.reply(`${Date.now() - timestamp}ms`);
   }
+});
 
-  if (cmd === '투두 벌금자') {
-    let result = await notionService.getTodayPenaltyList();
-    await msg.reply(result);
-  }
+//팀원
+server.on('message', async (msg) => {
+  if (!msg.content.startsWith(prefix)) return;
 
-  if (cmd === '투두 리스트') {
-    let result = await notionService.getListTodoWriters();
-    await msg.reply(result);
-  }
+  const args = msg.content.split(' ');
+  const cmd = args.shift()?.slice(prefix.length);
 
   if (cmd === '팀원') {
+    const timestamp = Date.now();
+
     let result = await notionService.getTeamMembers();
+
     await msg.reply(result);
+    msg.reply(`${Date.now() - timestamp}ms`);
   }
-
-  //TODO: 메세지를 받으면 특정 룸에 리플라이 하는 방법
-
-  // else if (cmd === 'kakaolink') {
-  //   msg.replyKakaoLink({
-  //     id: "00000", // 여기에 카카오 디벨로퍼 메시지 템플릿 코드 입력
-  //     args: {},
-  //   });
-  // }
 });
+//투두리스트;
+server.on('message', async (msg) => {
+  if (!msg.content.startsWith(prefix)) return;
+
+  const args = msg.content.split(' ');
+  const cmd = args.shift()?.slice(prefix.length);
+
+  if (cmd === '투두리스트') {
+    const timestamp = Date.now();
+
+    let result = await notionService.getListTodoWriters();
+
+    await msg.reply(JSON.stringify(result));
+    msg.reply(`${Date.now() - timestamp}ms`);
+  }
+});
+
+//투두벌금
+server.on('message', async (msg) => {
+  if (!msg.content.startsWith(prefix)) return;
+
+  const args = msg.content.split(' ');
+  const cmd = args.shift()?.slice(prefix.length);
+
+  if (cmd === '투두벌금') {
+    const timestamp = Date.now();
+
+    const currentTime = new Date();
+    // const currentKoreaTime =
+    //   currentTime.getHours() + (-1 * currentTime.getTimezoneOffset()) / 60;
+
+    let result = await notionService.getTodayPenaltyList();
+
+    // console.log(timestamp, '--timestamp--');
+    // console.log(currentTime, '--newDate--');
+    // console.log(currentTime.getTimezoneOffset());
+    // console.log(currentTime.getHours());
+
+    if (currentTime.getHours() < 14) {
+      await msg.reply('아직 14:00 안됨 얼렁 쓰세여');
+      msg.reply(`${Date.now() - timestamp}ms`);
+    }
+    if (result.length > 0) {
+      await msg.reply(JSON.stringify(result));
+      msg.reply(`${Date.now() - timestamp}ms`);
+    }
+    if (result.length === 0) {
+      await msg.reply('금일 벌금자 없음');
+      msg.reply(`${Date.now() - timestamp}ms`);
+    }
+  }
+});
+
+// else if (cmd === 'kakaolink') {
+//   msg.replyKakaoLink({
+//     id: "00000", // 여기에 카카오 디벨로퍼 메시지 템플릿 코드 입력
+//     args: {},
+//   });
+// }
+// });
 
 server.start(3000);
--- src/services/notionService.js
+++ src/services/notionService.js
@@ -56,16 +56,17 @@ const getTodayPenaltyList = async (todoWriters, teamMembers) => {
     .map((name) => '@' + name);
 };
 
-(async () => {
-  const todoWriters = await getListTodoWriters();
-  console.log(todoWriters, '--ToDoWriters--');
+// ** Notion API function output test **
+// (async () => {
+//   const todoWriters = await getListTodoWriters();
+//   console.log(todoWriters, '--ToDoWriters--');
 
-  const teamMembers = await getTeamMembers();
-  console.log(teamMembers, '--TeamMembers--');
+//   const teamMembers = await getTeamMembers();
+//   console.log(teamMembers, '--TeamMembers--');
 
-  const todayPenaltyList = await getTodayPenaltyList(todoWriters, teamMembers);
-  console.log(todayPenaltyList, '--PenaltyList--');
-})();
+//   const todayPenaltyList = await getTodayPenaltyList(todoWriters, teamMembers);
+//   console.log(todayPenaltyList, '--PenaltyList--');
+// })();
 
 module.exports = {
   getTeamMembers,
