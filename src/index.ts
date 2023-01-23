1a047ecb5af744444a78f1bddbb0db6282cfcbc9
whrod <whrodtube@gmail.com>
2023-01-24 01:22:42 +0900

    Feat : 오픈톡방 알림에 따른 알람 기능 구현

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 /* TODO:
 - 알람 기능
 - 중복 코드 간소화 (mapping)
-- 상수 enum화
-- 에러핸들링(배열.length === 0일때, promise)
+- 상수 관리
+- 에러핸들링(promise)
 - 카카오링크
 - @인물태그
 - 주석삭제
 */
 
-import { Server, Message } from '@remote-kakao/core';
+import { Server } from '@remote-kakao/core';
 import LoggerPlugin from './plugins/logger';
 
 //FIXME: 카카오링크 사용시
@@ -25,22 +25,50 @@ server.usePlugin(LoggerPlugin);
 // server.usePlugin(KakaoLinkPlugin, config);
 
 server.on('message', async (msg) => {
-  console.log(msg);
   if (!msg.content.startsWith(prefix)) return;
 
   const args = msg.content.split(' ');
   const cmd = args.shift()?.slice(prefix.length);
 
+  //pingTest
   if (cmd === 'ping') {
     const timestamp = Date.now();
 
     await msg.reply('Pong!');
     msg.reply(`${Date.now() - timestamp}ms`);
   }
-  //A방의 커맨드를 B방에서 응답하기
-  if (msg.room === 'test' && cmd === 'test') {
+
+  //오픈톡방 09:00 알람에 따른 응답 메세지
+  //TODO: 개발 관련 기사 크롤링해서 공유하기
+  if (msg.room === 'KCbot' && cmd === 'morning9:00') {
     const timestamp = Date.now();
-    msg.reply(`${Date.now() - timestamp}ms`, 'test2');
+    await msg.reply('굿모닝:) 투두리스트 작성해주세요!', '취업뽀개기');
+    msg.reply(
+      'https://www.notion.so/42e60fb224c74b748c1b30e1cda3fba6',
+      '취업뽀개기'
+    );
+    msg.reply(`${Date.now() - timestamp}ms`);
+  }
+
+  //오픈톡방 14:01 알람에 따른 응답 메세지
+  if (msg.room === 'KCbot' && cmd === 'afternoon14:01') {
+    const timestamp = Date.now();
+
+    let result = await notionService.getTodayPenaltyList();
+
+    try {
+      if (result.length > 0) {
+        msg.reply('삼천원 입금 ㄱㄱ', '취업뽀개기');
+        msg.reply(result.toString().replaceAll(',', '\n'), '취업뽀개기');
+        msg.reply(`${Date.now() - timestamp}ms`);
+      }
+      if (result.length === 0) {
+        msg.reply('금일 벌금자 없음', '취업뽀개기'); // i18n
+        msg.reply(`${Date.now() - timestamp}ms`);
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   //팀원 리스트
@@ -57,13 +85,15 @@ server.on('message', async (msg) => {
   }
 
   //팀원들 투두리스트(이름(작성시간):url)
-  //TODO: length===0일때 처리
   if (cmd === '투두리스트') {
     const timestamp = Date.now();
 
     let result = await notionService.getListTodoWriters();
 
     try {
+      if (result.length === 0) {
+        msg.reply('아직 아무도 안씀!!');
+      }
       msg.reply(result.toString().replaceAll(',', '\n'));
       msg.reply(`${Date.now() - timestamp}ms`);
     } catch (err) {
